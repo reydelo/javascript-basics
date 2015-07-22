4122e7cf07dfea8891fb2722ae0508c7d291cae1
reydelo <reyna.deloge@gmail.com>
2015-07-22 11:40:57 -0600

    write functions for sums of prices

--- js-iteration/js/dom.js
+++ js-iteration/js/dom.js
@@ -9,3 +9,25 @@ Hints for the first problem:
     - takes the value returned by the `sum` function and displays it on the page
 
 */
+var prices = document.getElementsByClassName("all-prices")[0].children;
+var sumSelected = document.getElementById("sum-selected");
+var sumAll = document.getElementById("sum-all");
+var countSelected = document.getElementById("count-selected");
+var avrAll = document.getElementById("average-all");
+var avrSelected = document.getElementById("average-selected");
+
+sumAll.onclick = function () {
+  console.log(sum());
+}
+countSelected.onclick = function () {
+  console.log(count());
+}
+sumSelected.onclick = function () {
+  console.log(someSum());
+}
+avrAll.onclick = function () {
+  console.log(sum()/prices.length);
+}
+avrSelected.onclick = function () {
+  console.log(someSum()/count());
+}
--- js-iteration/js/logic.js
+++ js-iteration/js/logic.js
@@ -10,3 +10,35 @@ Hints for the first problem:
 NOTE: use a `for` loop to make this happen.
 
 */
+
+// **Prices (use `while` loops here)**
+
+// Users can see the sum of all prices
+var sum = function () {
+  var total = 0;
+  for (var i = 0; i < prices.length; i++) {
+    total += parseInt(prices[i].value);
+  }
+  return total;
+}
+// Users can see the number of prices that are selected (the count)
+var count = function () {
+  var count = 0;
+  for (i in prices) {
+    if (prices[i].selected === true) {
+      count ++;
+    }
+  }
+  return count;
+}
+// Users can see the sum of the selected prices
+var someSum = function () {
+  var total = 0;
+  for (i in prices) {
+    if (prices[i].selected === true) {
+      total += parseInt(prices[i].value);
+    }
+  }
+  return total;
+}
+// Users can see the average of all of the prices
